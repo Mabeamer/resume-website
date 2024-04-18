7f8a26649517ca2e01c32ac76d2939267cf165f7
Mio <mabeamer01@gmail.com>
2024-04-18 14:48:12 +0000

    removing cluttered files

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, send_from_directory
 from ytdl import *
 import sys
 
@@ -18,9 +18,18 @@ def dl(name=None):
         print(userText)
         converter(userText)
         #send to /download url to give user download option
-        
+        return redirect("/dl", code=301)  
         
 
     #if link gets sent over
     #change website to have download button clickable and returns file
     return render_template('webdl.html', name=name, static_folder='static')
+
+@app.route('/dl', methods=['GET','POST'])
+def fileDl(name=None):
+    print("WE ARE GOING TO DOWNLOAD THIS FILE")
+    #incomplete 
+    if request.method == "POST":
+        fileName = ''
+        return send_from_directory(,fileName) 
+    return render_template('dl.html' name=name, static_folder='static')
