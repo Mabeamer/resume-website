a23e6595bf65573caa66b927008a559d2b807936
Mio <mabeamer01@gmail.com>
2024-06-16 20:46:26 +0000

    fixing picture frame

--- static/script.js
+++ static/script.js
@@ -1,28 +1,3 @@
 console.log('hello');
 
 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var testImg = document.getElementById("img01");
-var myImg = "img0";
-for (let i= 0; i < 10; i++){
-  var img = document.getElementById(myImg.concat(i));
-  console.log(testImg);
-}
-  var modalImg = document.getElementById("img01");
-  var captionText = document.getElementById("caption");
-  img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-    console.log('world');
-  }
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
